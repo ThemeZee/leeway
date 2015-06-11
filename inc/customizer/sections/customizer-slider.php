f8533c3c9cf0ca71cf75979e378807d531a23d11
Thomas W <netzberufler@googlemail.com>
2015-06-11 16:36:42 +0200

    new featured content class and number of posts setting

--- inc/customizer/sections/customizer-slider.php
+++ inc/customizer/sections/customizer-slider.php
@@ -12,7 +12,6 @@ function leeway_customize_register_slider_settings( $wp_customize ) {
 	// Add Sections for Slider Settings
 	$wp_customize->add_section( 'leeway_section_slider', array(
         'title'    => __( 'Post Slider', 'leeway' ),
-		'description' => __( 'The slideshow displays your featured posts, which you can configure on the "Featured Content" section above.', 'leeway' ),
         'priority' => 50,
 		'panel' => 'leeway_options_panel' 
 		)
@@ -65,7 +64,44 @@ function leeway_customize_register_slider_settings( $wp_customize ) {
 		'priority' => 3
 		)
 	);
+	
+	// Select Featured Posts
+	$wp_customize->add_setting( 'leeway_theme_options[featured_posts_header]', array(
+        'default'           => '',
+		'type'           	=> 'option',
+        'transport'         => 'refresh',
+        'sanitize_callback' => 'esc_attr'
+        )
+    );
+    $wp_customize->add_control( new Leeway_Customize_Header_Control(
+        $wp_customize, 'leeway_control_featured_posts_header', array(
+            'label' => __( 'Select Featured Posts', 'leeway' ),
+            'section' => 'leeway_section_slider',
+            'settings' => 'leeway_theme_options[featured_posts_header]',
+            'priority' => 4,
+			'active_callback' => 'leeway_slider_activated_callback'
+            )
+        )
+    );
+	$wp_customize->add_setting( 'leeway_theme_options[featured_posts_description]', array(
+        'default'           => '',
+		'type'           	=> 'option',
+        'transport'         => 'refresh',
+        'sanitize_callback' => 'esc_attr'
+        )
+    );
+    $wp_customize->add_control( new Leeway_Customize_Description_Control(
+        $wp_customize, 'leeway_control_featured_posts_description', array(
+			'label'    => __( 'The slideshow displays all your featured posts. You can easily feature posts by a tag of your choice.', 'leeway' ),
+            'section' => 'leeway_section_slider',
+            'settings' => 'leeway_theme_options[featured_posts_description]',
+            'priority' => 5,
+			'active_callback' => 'leeway_slider_activated_callback'
+            )
+        )
+    );
 
+	// Add Slider Animation Setting
 	$wp_customize->add_setting( 'leeway_theme_options[slider_animation]', array(
         'default'           => 'horizontal',
 		'type'           	=> 'option',
@@ -78,7 +114,8 @@ function leeway_customize_register_slider_settings( $wp_customize ) {
         'section'  => 'leeway_section_slider',
         'settings' => 'leeway_theme_options[slider_animation]',
         'type'     => 'radio',
-		'priority' => 4,
+		'priority' => 9,
+		'active_callback' => 'leeway_slider_activated_callback',
         'choices'  => array(
             'horizontal' => __( 'Horizontal Slider', 'leeway' ),
             'fade' => __( 'Fade Slider', 'leeway' )
