f3ebcea83a985fb50238f7476c52e920e58ccd3b
Thomas W <netzberufler@googlemail.com>
2015-07-13 14:21:46 +0200

    added the_archive_title function

--- archive.php
+++ archive.php
@@ -8,25 +8,19 @@
 		
 		<section id="content" class="primary" role="main">
 
-		<h2 id="date-title" class="archive-title">
-			<?php // Display Archive Title
-			if ( is_date() ) :
-				printf( __( 'Monthly Archives: %s', 'leeway' ), '<span>' . get_the_date( _x( 'F Y', 'date format of monthly archives', 'leeway' ) ) . '</span>' );
-			else :
-				_e( 'Archives', 'leeway' );
-			endif;
-			?>
-		</h2>
-		
-		<?php if (have_posts()) : while (have_posts()) : the_post();
-		
-			get_template_part( 'content', $theme_options['posts_length'] );
-		
-			endwhile;
+			<header class="page-header">
+				<?php the_archive_title( '<h2 class="archive-title">', '</h2>' ); ?>
+			</header>
 			
-		leeway_display_pagination();
+			<?php if (have_posts()) : while (have_posts()) : the_post();
+			
+				get_template_part( 'content', $theme_options['posts_length'] );
+			
+				endwhile;
+				
+			leeway_display_pagination();
 
-		endif; ?>
+			endif; ?>
 			
 		</section>
 		
--- search.php
+++ search.php
@@ -8,12 +8,13 @@
 		
 		<section id="content" class="primary" role="main">
 		
-		<?php if (have_posts()) : ?>
-			<h2 id="search-title" class="archive-title">
-				<?php printf( __( 'Search Results for: %s', 'leeway' ), '<span>' . get_search_query() . '</span>' ); ?>
-			</h2>
-		
-		<?php while (have_posts()) : the_post();
+			<header class="page-header">
+				<h2 id="search-title" class="archive-title">
+					<?php printf( __( 'Search Results for: %s', 'leeway' ), '<span>' . get_search_query() . '</span>' ); ?>
+				</h2>
+			</header>
+			
+		<?php if (have_posts()) : while (have_posts()) : the_post();
 		
 				get_template_part( 'content', $theme_options['posts_length'] );
 		
@@ -23,11 +24,7 @@
 
 		else : ?>
 
-			<h2 id="search-title" class="archive-title">
-				<?php printf( __( 'Search Results for: %s', 'leeway' ), '<span>' . get_search_query() . '</span>' ); ?>
-			</h2>
-			
-			<div class="post">
+			<div class="type-page">
 				
 				<div class="entry">
 					<p><?php _e('No matches. Please try again, or use the navigation menus to find what you search for.', 'leeway'); ?></p>
