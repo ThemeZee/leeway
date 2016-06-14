7295b8b70a895acd1f876582712374ac03507f0e
Thomas W <netzberufler@googlemail.com>
2016-06-14 11:39:47 +0200

    removed prefixing of common scripts and added scroll method for infinite scroll

--- functions.php
+++ functions.php
@@ -17,11 +17,11 @@ function leeway_enqueue_scripts() {
 	wp_enqueue_style( 'leeway-stylesheet', get_stylesheet_uri(), array(), $theme_version );
 	
 	// Register Genericons
-	wp_enqueue_style( 'leeway-genericons', get_template_directory_uri() . '/css/genericons/genericons.css', array(), '3.4.1' );
+	wp_enqueue_style( 'genericons', get_template_directory_uri() . '/css/genericons/genericons.css', array(), '3.4.1' );
 	
 	// Register and Enqueue HTML5shiv to support HTML5 elements in older IE versions
-	wp_enqueue_script( 'leeway-html5shiv', get_template_directory_uri() . '/js/html5shiv.min.js', array(), '3.7.3' );
-	wp_script_add_data( 'leeway-html5shiv', 'conditional', 'lt IE 9' );
+	wp_enqueue_script( 'html5shiv', get_template_directory_uri() . '/js/html5shiv.min.js', array(), '3.7.3' );
+	wp_script_add_data( 'html5shiv', 'conditional', 'lt IE 9' );
 
 	// Register and enqueue navigation.js
 	wp_enqueue_script( 'leeway-jquery-navigation', get_template_directory_uri() . '/js/navigation.js', array( 'jquery' ), '20160421' );
@@ -33,10 +33,10 @@ function leeway_enqueue_scripts() {
 		wp_enqueue_style( 'leeway-flexslider', get_template_directory_uri() . '/css/flexslider.css', array(), '20160421' );
 
 		// FlexSlider JS
-		wp_enqueue_script( 'leeway-flexslider', get_template_directory_uri() .'/js/jquery.flexslider-min.js', array( 'jquery' ), '2.6.0' );
+		wp_enqueue_script( 'flexslider', get_template_directory_uri() .'/js/jquery.flexslider-min.js', array( 'jquery' ), '2.6.0' );
 
 		// Register and enqueue slider.js
-		wp_enqueue_script( 'leeway-post-slider', get_template_directory_uri() .'/js/slider.js', array( 'leeway-flexslider' ), '2.6.0' );
+		wp_enqueue_script( 'leeway-post-slider', get_template_directory_uri() .'/js/slider.js', array( 'flexslider' ), '2.6.0' );
 
 	endif;
 
