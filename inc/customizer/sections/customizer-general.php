c3e8986e99be556764310b81cb3f178b49854993
Thomas W <kontakt@themecoder.de>
2018-04-13 17:36:57 +0200

    removed option to deactivate Google fonts

--- inc/customizer/sections/customizer-general.php
+++ inc/customizer/sections/customizer-general.php
@@ -13,10 +13,10 @@ function leeway_customize_register_general_settings( $wp_customize ) {
 	$wp_customize->add_section( 'leeway_section_general', array(
         'title'    => esc_html__( 'General Settings', 'leeway' ),
         'priority' => 10,
-		'panel' => 'leeway_options_panel' 
+		'panel' => 'leeway_options_panel'
 		)
 	);
-	
+
 	// Add Settings and Controls for Layout
 	$wp_customize->add_setting( 'leeway_theme_options[layout]', array(
         'default'           => 'right-sidebar',
@@ -37,40 +37,5 @@ function leeway_customize_register_general_settings( $wp_customize ) {
 			)
 		)
 	);
-	
-	// Add Default Fonts Header
-	$wp_customize->add_setting( 'leeway_theme_options[default_fonts]', array(
-        'default'           => '',
-		'type'           	=> 'option',
-        'transport'         => 'refresh',
-        'sanitize_callback' => 'esc_attr'
-        )
-    );
-    $wp_customize->add_control( new Leeway_Customize_Header_Control(
-        $wp_customize, 'leeway_control_default_fonts', array(
-            'label' => esc_html__( 'Default Fonts', 'leeway' ),
-            'section' => 'leeway_section_general',
-            'settings' => 'leeway_theme_options[default_fonts]',
-            'priority' => 2
-            )
-        )
-    );
-	
-	// Add Settings and Controls for Deactivate Google Font setting
-	$wp_customize->add_setting( 'leeway_theme_options[deactivate_google_fonts]', array(
-        'default'           => false,
-		'type'           	=> 'option',
-        'transport'         => 'refresh',
-        'sanitize_callback' => 'leeway_sanitize_checkbox'
-		)
-	);
-    $wp_customize->add_control( 'leeway_control_deactivate_google_fonts', array(
-        'label'    => esc_html__( 'Deactivate Google Fonts in case your language is not compatible.', 'leeway' ),
-        'section'  => 'leeway_section_general',
-        'settings' => 'leeway_theme_options[deactivate_google_fonts]',
-        'type'     => 'checkbox',
-		'priority' => 3
-		)
-	);
-	
+
 }
