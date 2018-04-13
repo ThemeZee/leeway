4ea3d7bad2a68fffca8a081e4e7efc989acbb0a6
Thomas W <kontakt@themecoder.de>
2018-04-13 17:15:29 +0200

    added local fonts

--- functions.php
+++ functions.php
@@ -44,39 +44,19 @@ function leeway_enqueue_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-
-	// Register and Enqueue Fonts
-	wp_enqueue_style( 'leeway-default-fonts', leeway_google_fonts_url(), array(), null );
-
 }
 
 
-// Retrieve Font URL to register default Google Fonts
-function leeway_google_fonts_url() {
-    $fonts_url = '';
+/**
+ * Enqueue custom fonts.
+ */
+function leeway_custom_fonts() {
 
-	// Get Theme Options from Database
-	$theme_options = leeway_theme_options();
+	// Register and Enqueue Theme Fonts.
+	wp_enqueue_style( 'leeway-custom-fonts', get_template_directory_uri() . '/css/custom-fonts.css', array(), '20180413' );
 
-	// Only embed Google Fonts if not deactivated
-	if ( ! ( isset($theme_options['deactivate_google_fonts']) and $theme_options['deactivate_google_fonts'] == true ) ) :
-
-		// Define Default Fonts
-		$font_families = array('Muli:400,700', 'Oswald');
-
-		// Set Google Font Query Args
-		$query_args = array(
-			'family' => urlencode( implode( '|', $font_families ) ),
-			'subset' => urlencode( 'latin,latin-ext' ),
-		);
-
-		// Create Fonts URL
-		$fonts_url = add_query_arg( $query_args, '//fonts.googleapis.com/css' );
-
-	endif;
-
-    return apply_filters( 'leeway_google_fonts_url', $fonts_url );
 }
+add_action( 'wp_enqueue_scripts', 'leeway_custom_fonts', 1 );
 
 
 // Setup Function: Registers support for various WordPress features
